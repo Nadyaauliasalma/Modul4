3fd00c4bd43a88a99c53d507300094f2f0aab085
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 06:49:41 +0700

    Add project files.

---
+++ KodeProduk.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4
+{
+    class KodeProduk
+    {
+        public string Produk Elektronik { get; set; }
+
+        public string K
+    }
+}
