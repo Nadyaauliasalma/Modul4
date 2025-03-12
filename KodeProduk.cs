566e4710ed70e6d8ec3841c612f32c0f4ca0333b
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 07:11:21 +0700

    Menambahkan class produk

--- KodeProduk.cs
+++ KodeProduk.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modul4;
 
 namespace Modul4
 {
     class KodeProduk
     {
-        public string Produk Elektronik { get; set; }
+        public string Produk { get; set; }
 
-        public string K
+        public string Kode { get; set; }
+
+        public static List<KodeProduk> GetKodeProduks()
+        {
+            return new List<KodeProduk>
+            {
+                new KodeProduk { Produk = "Laptop", Kode = "E100" },
+                new KodeProduk { Produk = "Smartphone", Kode = "E101" },
+                new KodeProduk { Produk = "Tablet", Kode = "E102" },
+                new KodeProduk { Produk = "Headset", Kode = "E103" },
+                new KodeProduk { Produk = "Keyboard", Kode = "E104" },
+                new KodeProduk { Produk = "Mouse", Kode = "E105" },
+                new KodeProduk { Produk = "Printer", Kode = "E106" },
+                new KodeProduk { Produk = "Monitor", Kode = "E107" },
+                new KodeProduk { Produk = "Smartwatch", Kode = "E108" },
+                new KodeProduk { Produk = "kamera", Kode = "E109" },
+            };
+        }
     }
 }
